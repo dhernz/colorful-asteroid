170826bb69ef53cf4919422a3cfbdd98cecde4eb
Doris Hernandez <d.hernz@gmail.com>
2015-08-22 13:38:48 -0700

    Adding db fix

--- public/models/database.js
+++ public/models/database.js
@@ -1,17 +1,7 @@
 var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/postgres';
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/icusmooth';
 
 var client = new pg.Client(connectionString);
 client.connect();
-var query = client.query(
-'CREATE TABLE topics
-(
- id serial NOT NULL,
- text character varying(160) NOT NULL,
- vote integer NOT NULL,
- CONSTRAINT topics_pkey PRIMARY KEY (id)
-)
-WITH (
- OIDS=FALSE
-);');
+var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
 query.on('end', function() { client.end(); });
