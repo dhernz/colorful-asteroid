955d0f2f0c5eae82d52746d2a857f861458e939b
Doris Hernandez <d.hernz@gmail.com>
2015-08-22 12:12:26 -0700

    added correct db schema

--- public/models/database.js
+++ public/models/database.js
@@ -3,5 +3,15 @@ var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/ic
 
 var client = new pg.Client(connectionString);
 client.connect();
-var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+var query = client.query(
+'CREATE TABLE topics
+(
+ id serial NOT NULL,
+ text character varying(160) NOT NULL,
+ vote integer NOT NULL,
+ CONSTRAINT topics_pkey PRIMARY KEY (id)
+)
+WITH (
+ OIDS=FALSE
+);');
 query.on('end', function() { client.end(); });
