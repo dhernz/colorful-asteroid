cbce7988c7bb4b1de247696e9de2b498ffb9f0e9
Doris Hernandez <d.hernz@gmail.com>
2015-08-22 12:24:20 -0700

    fixed db

--- public/models/database.js
+++ public/models/database.js
@@ -1,5 +1,5 @@
 var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/icusmooth';
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/postgres';
 
 var client = new pg.Client(connectionString);
 client.connect();
